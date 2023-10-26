6f4f0c5e426c0126e5a37715ada25b3ddd1a8b7f
OlisaPaul <odirahchukwumma28@gmail.com>
2023-10-26 19:00:11 +0100

    Feature: Added new endpoints for getting and updating threshold Distance

---
+++ src/controllers/distanceThreshold.controllers.js
@@ -0,0 +1,58 @@
+const { DistanceThreshold } = require("../model/distanceThreshold.model");
+const distanceThresholdService = require("../services/distanceThreshold.services");
+const { MESSAGES } = require("../common/constants.common");
+const { successMessage } = require("../common/messages.common");
+const { validLocationType } = require("../model/entry.model").joiValidator;
+
+class DistanceThresholdController {
+  async getStatus(req, res) {
+    res.status(200).send({ message: MESSAGES.DEFAULT, success: true });
+  }
+
+  //Create a new distanceThreshold
+  async createDistanceThreshold(req, res) {
+    const {
+      Scanned,
+      TakenFromShop,
+      TakenToShop,
+      PickupFromDealership,
+      DropOffCompleted,
+    } = req.body;
+
+    const distanceThreshold = new DistanceThreshold({
+      Scanned,
+      TakenFromShop,
+      TakenToShop,
+      PickupFromDealership,
+      DropOffCompleted,
+    });
+
+    await distanceThreshold.save();
+
+    res.send(successMessage(MESSAGES.FETCHED, distanceThreshold));
+  }
+  async getDistanceThreshold(req, res) {
+    const distanceThreshold =
+      await distanceThresholdService.getDistanceThreshold();
+    if (!distanceThreshold)
+      return res.status(404).send(errorMessage("distanceThreshold"));
+
+    res.send(successMessage(MESSAGES.UPDATED, distanceThreshold));
+  }
+
+  async updateDistanceThreshold(req, res) {
+    const distanceThreshold =
+      await distanceThresholdService.getDistanceThreshold();
+    if (!distanceThreshold)
+      return res.status(404).send(errorMessage("distanceThreshold"));
+
+    for (const locationType of Object.keys(req.body))
+      distanceThreshold[locationType] = req.body[locationType];
+
+    await distanceThreshold.save();
+
+    res.send(successMessage(MESSAGES.UPDATED, distanceThreshold));
+  }
+}
+
+module.exports = new DistanceThresholdController();
