a8d7a2a427056b3d1639e7f34cdd06b2c6a9b251
OlisaPaul <odirahchukwumma28@gmail.com>
2023-11-03 17:14:55 +0100

    fix - bug

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -3,7 +3,7 @@ const webhookControllers = require("../controllers/webhook.controllers");
 const router = express.Router();
 const asyncMiddleware = require("../middleware/async.middleware");
 
-router.post("/", qboAsyncMiddleware(webhookControllers.webhook));
+router.post("/", asyncMiddleware(webhookControllers.webhook));
 router.post(
   "/stripe-acoounts",
   express.raw({ type: "application/json" }),
