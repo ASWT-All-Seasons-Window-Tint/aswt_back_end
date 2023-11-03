5d8aa4e6510d09b76184af89ac2153ff7157ab1c
OlisaPaul <odirahchukwumma28@gmail.com>
2023-11-03 16:39:35 +0100

    Fix: Handlind un handled rejections

--- src/routes/oauth2.routes.js
+++ src/routes/oauth2.routes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const asyncMiddleware = require("../middleware/async.middleware");
 const ouath2Controller = require("../controllers/oauth2.controllers");
 
-router.get("/", ouath2Controller.start);
-router.get("/requestToken", ouath2Controller.requestToken);
-router.get("/callback", ouath2Controller.callback);
+router.get("/", asyncMiddleware(ouath2Controller.start));
+router.get("/requestToken", asyncMiddleware(ouath2Controller.requestToken));
+router.get("/callback", asyncMiddleware(ouath2Controller.callback));
 
-router.get("/start", ouath2Controller.startStripe);
-router.get("/stripe", ouath2Controller.stripeAuthorize);
-router.get("/callback/stripe", ouath2Controller.stripeCallback);
+router.get("/start", asyncMiddleware(ouath2Controller.startStripe));
+router.get("/stripe", asyncMiddleware(ouath2Controller.stripeAuthorize));
+router.get(
+  "/callback/stripe",
+  asyncMiddleware(ouath2Controller.stripeCallback)
+);
 
 module.exports = router;
