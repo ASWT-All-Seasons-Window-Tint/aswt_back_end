b76480253cfc02dac304bd5c58d975e9b0f23558
OlisaPaul <odirahchukwumma28@gmail.com>
2023-12-21 19:17:14 +0100

    Feature - entry made sure services is added the right way

--- src/controllers/entry.controllers.js
+++ src/controllers/entry.controllers.js
@@ -165,23 +165,8 @@ class EntryController {
 
     if (checkErr.message) return badReqResponse(res, checkErr.message);
 
-    if (priceBreakdown.length < 1) return serverErrResponse(res);
-
-    const servicesWithoutDealershipPrice = priceBreakdown.filter(
-      (price) => !price.dealership
-    );
-    if (servicesWithoutDealershipPrice.length > 0) {
-      const serviceNames = servicesWithoutDealershipPrice.map(
-        (service) => service.serviceName
-      );
-
-      return notFoundResponse(
-        res,
-        `There is no dealership price for these services: (${serviceNames.join(
-          ", "
-        )})`
-      );
-    }
+    const checkDealershipPrice = this.checkDealershipPrice(res, priceBreakdown);
+    if (checkDealershipPrice) return;
 
     const updateCarDetailsResult = entryService.updateCarDetails(
       entry,
@@ -287,6 +272,26 @@ class EntryController {
 
     res.send(successMessage(MESSAGES.UPDATED, carDetails));
   };
+
+  checkDealershipPrice(res, priceBreakdown) {
+    if (priceBreakdown.length < 1) return serverErrResponse(res);
+
+    const servicesWithoutDealershipPrice = priceBreakdown.filter(
+      (price) => !price.dealership
+    );
+    if (servicesWithoutDealershipPrice.length > 0) {
+      const serviceNames = servicesWithoutDealershipPrice.map(
+        (service) => service.serviceName
+      );
+
+      return notFoundResponse(
+        res,
+        `There is no dealership price for these services: (${serviceNames.join(
+          ", "
+        )})`
+      );
+    }
+  }
 
   getDelay() {
     const currentDate = new Date();
@@ -833,20 +838,36 @@ class EntryController {
     let lineId = entryService.sumPriceBreakdownLength(entry);
 
     const { priceBreakdown: newPriceBreakdown, checkErr } =
-      !entry.isFromAppointment
-        ? await entryService.getPriceForService(
-            [service],
+      entry.isFromAppointment && !entry.isFromDealership
+        ? { priceBreakdown: [], checkErr: {} }
+        : await entryService.getPriceForService(
+            [serviceId],
             entry.customerId,
             carWithVin.category,
             lineId,
             serviceDetails
-          )
-        : { priceBreakdown: [], checkErr: {} };
+          );
+
+    const priceBreakdown = carWithVin.priceBreakdown;
+    const totalPriceBreakdown = [...priceBreakdown, ...newPriceBreakdown];
+
+    if (entry.isFromDealership) {
+      const checkDealershipPrice = this.checkDealershipPrice(
+        res,
+        newPriceBreakdown
+      );
+      if (checkDealershipPrice) return;
+
+      carWithVin.price =
+        entryService.calculateServicePriceDoneforCar(totalPriceBreakdown);
+
+      const totalPrice = entryService.getTotalprice(entry.invoice);
+      entry.invoice.totalPrice = totalPrice;
+    }
 
     if (checkErr.message) return badReqResponse(res, checkErr.message);
 
-    const priceBreakdown = carWithVin.priceBreakdown;
-    carWithVin.priceBreakdown = [...priceBreakdown, ...newPriceBreakdown];
+    carWithVin.priceBreakdown = totalPriceBreakdown;
 
     const updatedCarWithVIn = await entryService.updateServicesDoneOnCar(
       carWithVin,
@@ -954,10 +975,12 @@ class EntryController {
             const { statusCode, message } =
               await invoiceControllers.updateInvoiceById(undefined, entry);
 
-            resultsError.message = message;
-            resultsError.code = statusCode;
+            if (message && statusCode) {
+              resultsError.message = message;
+              resultsError.code = statusCode;
 
-            if (statusCode) return resultsError;
+              throw new Error(message);
+            }
           }
         }
 
