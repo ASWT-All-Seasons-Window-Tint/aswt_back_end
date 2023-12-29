4235cce9874da74f39f6bd7c87715a501e6253e1
OlisaPaul <odirahchukwumma28@gmail.com>
2023-12-29 09:35:22 +0100

    fix: bug

--- src/controllers/entry.controllers.js
+++ src/controllers/entry.controllers.js
@@ -294,11 +294,11 @@ class EntryController {
 
     // Set the next 24 hours to 11:59 PM CST
     const next24Hours = new Date();
-    next24Hours.setHours(11, 59, 0, 0); // Set hours to 23 (11 PM), minutes to 59, seconds to 0, and milliseconds to 0
+    next24Hours.setHours(23, 59, 0, 0); // Set hours to 23 (11 PM), minutes to 59, seconds to 0, and milliseconds to 0
 
     const delay = next24Hours.getTime() - currentDate.getTime();
 
-    return 120000;
+    return delay;
   }
 
   async addCarGeoLocation(req, res) {
