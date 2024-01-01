83f63c08cad1be1b9473b3cf9acef14306213cec
OlisaPaul <odirahchukwumma28@gmail.com>
2024-01-01 08:28:30 +0100

    modification: removed cron job for automatically sending all unpaid invoices

--- src/startup/cron.startup.js
+++ src/startup/cron.startup.js
@@ -4,11 +4,11 @@ const { sendUpaidInvoices } = require("../controllers/invoice.controllers");
 function startScheduledJob() {
   // Schedule the function to run on the last day of each month at a specific time (e.g., 00:00)
   cron.schedule(
-    "50 4 1,15 * *",
+    "* * * * *",
     async () => {
       try {
-        await sendUpaidInvoices();
-        console.log("Scheduled task executed successfully.");
+        // await sendUpaidInvoices();
+        console.log("Scheduled task executed.");
       } catch (error) {
         console.error("Error executing scheduled task:", error);
       }
