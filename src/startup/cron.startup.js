a1c58a7ffbddb23105f018e08f2c3c13b4e96117
OlisaPaul <odirahchukwumma28@gmail.com>
2024-01-05 05:26:25 +0100

    modification: for debugging ability to see logs of invoice that where not sent

--- src/startup/cron.startup.js
+++ src/startup/cron.startup.js
@@ -5,7 +5,7 @@ const entryService = require("../services/entry.services");
 function startScheduledJob() {
   // Schedule the function to run on the last day of each month at a specific time (e.g., 00:00)
   cron.schedule(
-    "59 23 * * *",
+    "1 22 * * *",
     async () => {
       const currentDate = new Date();
       try {
@@ -21,6 +21,10 @@ function startScheduledJob() {
         }
       } catch (error) {
         console.error("Error executing scheduled task:", error);
+        if (error.Fault) {
+          if (error.Fault.Error)
+            error.Fault.Error.map((err) => console.log(err));
+        }
       }
     },
     {
